6e8ffa3c78a9805edd2556f7cd3a91b95a29a692
Sitam <sitammeur@gmail.com>
2024-11-23 01:32:16 +0530

    deployment ready to hf spaces

---
+++ app.py
@@ -0,0 +1,64 @@
+# Importing the requirements
+import warnings
+warnings.filterwarnings("ignore")
+
+import gradio as gr
+from src.app.predict_pose import predict_pose
+
+
+# Image and input parameters
+image = gr.Image(type="pil", label="Image")
+confidence_threshold = gr.Slider(
+    minimum=0, maximum=1, step=0.01, value=0.25, label="Confidence threshold"
+)
+iou_threshold = gr.Slider(
+    minimum=0, maximum=1, step=0.01, value=0.45, label="IoU threshold"
+)
+max_detections = gr.Slider(
+    minimum=1, maximum=300, step=1, value=300, label="Max detections"
+)
+model_name = gr.Radio(
+    choices=[
+        "yolo11n-pose.pt",
+        "yolo11s-pose.pt",
+        "yolo11m-pose.pt",
+        "yolo11l-pose.pt",
+        "yolo11x-pose.pt",
+    ],
+    label="Model name",
+    value="yolo11n-pose.pt",
+)
+
+# Output image
+pose_image = gr.Image(type="pil", label="Output Image")
+
+# Examples for the interface
+examples = [
+    ["images/posing-sample-image3.jpg", 0.25, 0.45, 300, "yolo11n-pose.pt"],
+    ["images/posing-sample-image4.jpg", 0.25, 0.45, 300, "yolo11s-pose.pt"],
+    ["images/posing-sample-image5.jpg", 0.25, 0.45, 300, "yolo11m-pose.pt"],
+    ["images/posing-sample-image1.jpg", 0.25, 0.45, 300, "yolo11l-pose.pt"],
+    ["images/posing-sample-image2.png", 0.25, 0.45, 300, "yolo11x-pose.pt"],
+]
+
+# Title, description, and article for the interface
+title = "YOLO11 Pose Estimation"
+description = "Gradio Demo for the YOLO11 Pose Estimation model. This model can detect and predict the poses of people in images. To use it, upload your image, select associated parameters, or use the default values, click 'Submit', or click one of the examples to load them. You can read more at the links below."
+article = "<p style='text-align: center'><a href='https://github.com/ultralytics/ultralytics' target='_blank'>Ultralytics GitHub</a> | <a href='https://docs.ultralytics.com/models/yolo11/' target='_blank'>Model Page</a></p>"
+
+
+# Launch the interface
+interface = gr.Interface(
+    fn=predict_pose,
+    inputs=[image, confidence_threshold, iou_threshold, max_detections, model_name],
+    outputs=pose_image,
+    examples=examples,
+    cache_examples=True,
+    cache_mode="lazy",
+    title=title,
+    description=description,
+    article=article,
+    theme="Nymbo/Nymbo_Theme",
+    flagging_mode="never",
+)
+interface.launch(debug=False)
---
+++ src/app/predict_pose.py
@@ -0,0 +1,67 @@
+import sys
+import PIL.Image as Image
+from ultralytics import YOLO
+import gradio as gr
+import spaces
+
+# Local imports
+from src.logger import logging
+from src.exception import CustomExceptionHandling
+
+
+@spaces.GPU
+def predict_pose(
+    img: str,
+    conf_threshold: float,
+    iou_threshold: float,
+    max_detections: int,
+    model_name: str,
+) -> Image.Image:
+    """
+    Predicts objects in an image using a YOLOv8 model with adjustable confidence and IOU thresholds.
+
+    Args:
+        - img (str or numpy.ndarray): The input image or path to the image file.
+        - conf_threshold (float): The confidence threshold for object detection.
+        - iou_threshold (float): The Intersection Over Union (IOU) threshold for non-max suppression.
+        - model_name (str): The name or path of the YOLOv8 model to be used for prediction.
+
+    Returns:
+        PIL.Image.Image: The image with predicted objects plotted on it.
+    """
+    try:
+        # Check if image is None
+        if img is None:
+            gr.Warning("Please provide an image.")
+
+        # Load the YOLO model
+        model = YOLO(model_name)
+
+        # Predict objects in the image
+        results = model.predict(
+            source=img,
+            conf=conf_threshold,
+            iou=iou_threshold,
+            max_det=max_detections,
+            show_labels=True,
+            show_conf=True,
+            imgsz=640,
+            half=True,
+            device="cuda:0",
+        )
+
+        # Plot the predicted objects on the image
+        for r in results:
+            im_array = r.plot()
+            im = Image.fromarray(im_array[..., ::-1])
+
+        # Log the successful prediction
+        logging.info("Pose estimated successfully.")
+
+        # Return the image
+        return im
+
+    # Handle exceptions that may occur during the process
+    except Exception as e:
+        # Custom exception handling
+        raise CustomExceptionHandling(e, sys) from e
